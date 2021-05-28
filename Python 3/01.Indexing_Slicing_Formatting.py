72e28123cabe8d75b227c7f530b271642f339500
Byeon Ui-Hyeok <bym4094@naver.com>
2021-05-28 17:01:56 +0900

    Indexing_Slicing_Formatting

--- Python 3/01.Indexing_Slicing_Formatting.py
+++ Python 3/01.Indexing_Slicing_Formatting.py
@@ -12,12 +12,14 @@ indexing_test_Year  = test_filename_Str [0:4] # 2021
 indexing_test_Month = test_filename_Str [5:7] # 05
 indexing_test_Day   = test_filename_Str [8: ] # 28
 
-print ( indexing_test_Year  ) # 2021
-print ( indexing_test_Month ) # 05
-print ( indexing_test_Day   ) # 28
-print ( indexing_test_Year + ' - ' + indexing_test_Month + ' - ' + indexing_test_Day )            # indexing + '-' indexing + '-' + indexing
-print ( '%s - %s - %s' % ( indexing_test_Year, indexing_test_Month, indexing_test_Day ) )         # formatting (1)
-print ( '{0} - {1} - {2}'.format(indexing_test_Year, indexing_test_Month, indexing_test_Day))     # formatting (2)
-print ( f'{indexing_test_Year} - {indexing_test_Month} - {indexing_test_Day}' )                   # formatting (3)
-print ( test_filename_Str.replace(' ', ' - ') )                                                   # function
+ # indexing + '-' indexing + '-' + indexing
+print ( indexing_test_Year + ' - ' + indexing_test_Month + ' - ' + indexing_test_Day )
+ # formatting (1)
+print ( '%s - %s - %s' % ( indexing_test_Year, indexing_test_Month, indexing_test_Day ) )         
+ # formatting (2)
+print ( '{0} - {1} - {2}'.format(indexing_test_Year, indexing_test_Month, indexing_test_Day))
+ # formatting (3)     
+print ( f'{indexing_test_Year} - {indexing_test_Month} - {indexing_test_Day}' )
+ # function
+print ( test_filename_Str.replace(' ', ' - ') )                                                   
 print ( indexing_test_Year, indexing_test_Month, indexing_test_Day, sep = ' - ')
