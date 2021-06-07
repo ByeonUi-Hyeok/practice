fd990c8aa65a212dafde7f9929c8bd625f45e6cc
Byeon Ui-Hyeok <bym4094@naver.com>
2021-06-07 12:38:15 +0900

    21.06.07 숫자 10처리가능

--- Python 3/14.charactor.py
+++ Python 3/14.charactor.py
@@ -1,23 +1,33 @@
-# 21.06.06
-# 문자열 처리 / 입력받은 문자열 숫자, 문자, 특수문자 구분하기
+# 21.06.07
 # 예외처리문 / 정규식표현 쓰기
-# 숫자 10처리하지 못함
 filename_ap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 filename_result = []
 filename_input = input()
 filename_nb = '0123456789'
 filename_ast = '*#'
 
-for i in filename_input:
+for i in range(len(filename_input)):
+    if filename_input[i] == '0' :
+        if i - 1 == -1 :
+            pass
+        elif i - 1 != -1:
+            if filename_input[i-1] == '1':
+                continue
+
+
     for j in range(10):
-        if i == str(j) :
-            #if i == '1':
-            filename_result.append(int(i))
+        if filename_input[i] == str(j) :
+            if filename_input[i] == '1' and filename_input[i+1] == '0' :
+                filename_result.append(int(filename_input[i:i+2]))
+                break
+            filename_result.append(int(filename_input[i]))
             break
-        elif i in filename_ap :
-            filename_result.append(i)
+        elif filename_input[i] in filename_ap :
+            filename_result.append(filename_input[i])
             break
-        elif i in filename_ast :
-            filename_result.append(i)
+        elif filename_input[i] in filename_ast :
+            filename_result.append(filename_input[i])
             break
 print(filename_result)
+
+# 숫자 10처리 가능
